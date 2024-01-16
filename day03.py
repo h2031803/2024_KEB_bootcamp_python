c62cccc5fe06ed3b50d6c6d8290c16a3b772a27a
h2031803 <h2031803@gmail.com>
2024-01-16 14:40:36 +0900

    v2.2 memorytime save coding(while)

--- day03.py
+++ day03.py
@@ -50,15 +50,18 @@
 # prime number
 number = int(input("Input number : "))
 is_prime = True # int -> bool
-i = 2
-while i < number:
-    if number % i == 0:
-        is_prime = False # remove +
-        break
-    i += 1
 
-#if cnt == 0 :
-if is_prime: # remove ==
-    print(f'{number} is prime number')
+if number<2:
+    print(f'{number} is NOT prime number!')
 else :
-    print(f'{number} is NOT prime number')
+    i = 2
+    while i < number:
+        if number % i == 0:
+            is_prime = False # remove +
+            break
+        i += 1
+    #if cnt == 0 :
+    if is_prime: # remove ==
+        print(f'{number} is prime number')
+    else :
+        print(f'{number} is NOT prime number')
