a5532a001cb1fae4952029b74bbc50c8a89bed86
h2031803 <h2031803@gmail.com>
2024-01-16 11:44:28 +0900

    v1.9

--- day03.py
+++ day03.py
@@ -32,4 +32,9 @@
 # print(course.index('KEB')) # .index = .find, rindex 도 동일기능
 # print(course.find('KEC')) # -1 return (can't find) /// index는 오류 발생
 
-
+subjects = "python c++ database linux"
+subject = input("수강신청과목 입력 : ")
+try:
+    print(f"해당 과목이 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.")
+except ValueError:
+    print('해당 과목 존재 x')
